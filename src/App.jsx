5e245d4606b838c4ebc5ad4009c134a85ffcd48b
Aixa Filsinger <macafil99@gmail.com>
2023-05-26 23:49:02 -0300

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import Ejercicio2 from './Components/Ejercicio2'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   
 
   return (
     <>
-      
+      <Ejercicio2 nuevoTitulo = "my Friend"/>
     </>
   )
 }
---
+++ src/Components/Ejercicio2.jsx
@@ -0,0 +1,11 @@
+
+
+const Ejercicio2 = ({nuevoTitulo}) => {
+    return (
+        <section>
+            <h1>Hello {nuevoTitulo}!</h1>
+        </section>
+    );
+};
+
+export default Ejercicio2;
